04696eb76a48e2754bd99ea7990ccf8271822372
Tuu <thientuu2911@gmail.com>
2024-07-07 20:40:18 +0700

    Fix css for the If node

--- src/pages/Workflow/components/tag/Tag.tsx
+++ src/pages/Workflow/components/tag/Tag.tsx
@@ -1,3 +1,5 @@
+import "./style.css";
+
 import { Flex, Tag, Tooltip, theme } from "antd";
 
 import { truncate } from "cypress/types/lodash";
@@ -7,6 +9,12 @@ const TagComponent = ({ data }: any) => {
 
   const formatText = (text: string) =>
     text.replaceAll("(a-z)([A-Z]+)", "$1 $2");
+
+  const validateCalculationChar = (char: string) => {
+    const allowedChars = "+-*/";
+
+    return allowedChars.includes(char);
+  };
 
   switch (data.typeNode) {
     case "ConditionSetup":
@@ -60,18 +68,46 @@ const TagComponent = ({ data }: any) => {
         </Tooltip>
       ));
     case "If":
+      const expressionTag = data.formula
+        .map((item: any) => {
+          if (
+            item.label === "(" ||
+            item.label === ")" ||
+            validateCalculationChar(item.label)
+          ) {
+            return `<span class="bracket">${item.label}</span>`;
+          } else {
+            if (item.value) {
+              switch (item.value.split("#")[0]) {
+                case "metadata":
+                  return `<span class="${item.value.split("#")[0]}">${
+                    item.label
+                  }</span>`;
+                case "operator":
+                  return `<span class="${item.value.split("#")[0]}">${
+                    item.label
+                  }</span>`;
+                case "next":
+                  return `<span class="${item.value.split("#")[0]}">${
+                    item.label
+                  }</span>`;
+                default:
+                  return `<span class="value">${item.label}</span>`;
+              }
+            } else {
+              return `<span class="value">${item}</span>`;
+            }
+          }
+        })
+        .join(" ");
       return (
         <Tooltip
-          title={
-            <>
-              <span>{formatText(data.expression)}</span>
-            </>
-          }
+          title={<span dangerouslySetInnerHTML={{ __html: expressionTag }} />}
           placement="bottom"
         >
           <Tag className={"truncate"} style={{ width: "100%" }}>
             <Flex gap={5}>
-              <span>{formatText(data.expression)}</span>
+              {<span dangerouslySetInnerHTML={{ __html: expressionTag }} />}
             </Flex>
           </Tag>
         </Tooltip>
